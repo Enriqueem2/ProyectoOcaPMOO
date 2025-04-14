704bd00c9f81f32de671bec5d89596d3d3ae65b5
enzod <enzod@192.168.1.168>
2025-04-14 12:40:23 +0200

    Se ha hecho la clase CasillaNormal.

--- JuegoProyecto/src/org/pmoo/juegoProyecto/CasillaNormal.java
+++ JuegoProyecto/src/org/pmoo/juegoProyecto/CasillaNormal.java
@@ -1,5 +1,13 @@
 package org.pmoo.juegoProyecto;
 
-public class CasillaNormal {
+public class CasillaNormal extends Casilla {
+	public CasillaNormal(int pnumCasilla) {
+        super(pnumCasilla);
+    }
 
+    @Override
+    public void aplicarEfecto(pJugador jugador, pJuego juego) {
+        // No hace nada
+    }
 }
+
