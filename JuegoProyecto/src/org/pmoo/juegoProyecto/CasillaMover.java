3a028e976e65187df79759c37214687242200389
enzod <enzod@10.247.57.10>
2025-04-14 13:04:36 +0200

    Se ha hecho la clase CasillaMover

--- JuegoProyecto/src/org/pmoo/juegoProyecto/CasillaMover.java
+++ JuegoProyecto/src/org/pmoo/juegoProyecto/CasillaMover.java
@@ -1,5 +1,24 @@
 package org.pmoo.juegoProyecto;
 
-public class CasillaMover {
+public class CasillaMover extends Casilla{
+	    private int destino;
+	    private boolean volverATirar;
 
-}
+	    public CasillaMover(int pNumCasilla, int pDestino, boolean pVolverATirar) {
+	        super(pNumCasilla);
+	        this.destino = pDestino;
+	        this.volverATirar = pVolverATirar;
+	    }
+
+	    @Override // Casillas Puentes, Dados y Ocas
+	    public void aplicarEfecto(Jugador pJugador, Juego pJuego) {
+	        pJugador.moverACasilla(destino);
+
+	        //Casillas Dados y Casillas Ocas
+	        if (volverATirar) {
+	            int tirada = pJugador.tirarDado();
+	            pJugador.avanzar(tirada, pJuego);
+	        }
+	    }
+	}
+
