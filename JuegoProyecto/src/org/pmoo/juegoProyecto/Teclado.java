218a9213a4f32d7edf60f22da5fdc658911820ad
enriq <enriq@Enrique>
2025-04-29 14:49:06 +0200

    Se ha creado la MAE teclado junto a los metodos leerEntero y leerNumeroJugadores

---
+++ JuegoProyecto/src/org/pmoo/juegoProyecto/Teclado.java
@@ -0,0 +1,48 @@
+package org.pmoo.juegoProyecto;
+import java.util.Scanner;
+
+public class Teclado {
+    private static Teclado miTeclado = null;
+    private Scanner sc;
+
+    private Teclado() {
+        sc = new Scanner(System.in);
+    }
+
+    public static Teclado getTeclado() {
+        if (miTeclado == null) {
+            miTeclado = new Teclado();
+        }
+        return miTeclado;
+    }
+
+    public int leerEntero() {
+        while (!sc.hasNextInt()) {
+            System.out.println("Por favor, introduce un número válido:");
+            sc.next(); 
+        }
+        int numero = sc.nextInt();
+        sc.nextLine(); // salta de linea
+        return numero;
+    }
+    
+    public int leerNumeroJugadores() {
+        int numJugadores = 0;
+        boolean valido = false;
+
+        while (!valido) {
+            System.out.print("Introduce el número de jugadores (entre 1 y 5): ");
+            numJugadores = leerEntero();  
+
+            if (numJugadores >= 2 && numJugadores <= 5) {
+                valido = true;
+            } else {
+                System.out.println("Número inválido. Debe estar entre 1 y 5.");
+            }
+        }
+
+        return numJugadores;
+    }
+
+    
+}
